3d4758758028013a6fa52bf5437ac6b54105212e
AKG3000 <91015223+AKG3000@users.noreply.github.com>
2024-10-03 12:03:08 +0530

    Utils + Deserialization

--- src/test/java/com/fancode/tests/DemoTest.java
+++ src/test/java/com/fancode/tests/DemoTest.java
@@ -9,6 +9,9 @@ import com.fancode.config.factory.ApiConfigFactory;
 import com.fancode.models.Geo;
 import com.fancode.models.Todo;
 import com.fancode.models.UserLocation;
+import com.fancode.tests.data.UserDataProvider;
+import com.fancode.utils.DeserializeUtils;
+import com.fancode.utils.TodoUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.restassured.RestAssured;
@@ -21,90 +24,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DemoTest {
-	public List<UserLocation> usersInFancodeCity = new ArrayList<>();
 
-	public List<UserLocation> deserializeUserResponse(String jsonResponse) throws IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<UserLocation> users = new ArrayList<>();
-		try {
-			users = objectMapper.readValue(jsonResponse,
-					objectMapper.getTypeFactory().constructCollectionType(List.class, UserLocation.class));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Unable to return users");
-		}
-		return users;
-	}
-
-	public List<Todo> deserializeTodoResponse(String jsonResponse) throws IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<Todo> users = new ArrayList<>();
-		try {
-			users = objectMapper.readValue(jsonResponse,
-					objectMapper.getTypeFactory().constructCollectionType(List.class, Todo.class));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Unable to return todos");
-		}
-		return users;
-	}
-
-	public static boolean isUserFromCityFancode(double latitude, double longitude) {
-		return latitude >= -40 && latitude <= 5 && longitude >= 5 && longitude <= 100;
-	}
-
-	@DataProvider(name = "userDataProvider")
-	public Object[] userDataProvider() {
-		int[] userIds = usersInFancodeCity.stream().mapToInt(UserLocation::getId).toArray();
-
-		Integer[] integerArray = Arrays.stream(userIds) // Create an IntStream from the int array
-				.boxed() // Box each int to Integer
-				.toArray(Integer[]::new); // Collect into an Integer[]
-
-// Step 3: Convert Integer[] to Object[]
-		return Arrays.copyOf(integerArray, integerArray.length, Object[].class);
-	}
-
-	public static List<UserLocation> filterUsersByGeo(List<UserLocation> users) {
-		return users.stream().filter(user -> {
-			Geo geo = user.getAddress().getGeo();
-			// Filter by lat and lng
-			double lat = geo.getLat();
-			double lng = geo.getLng();
-			return (lat >= -40 && lat <= 5 && lng >= 5 && lng <= 100);
-		}).collect(Collectors.toList());
-	}
-
-	public static double calculateCompletedTaskPercentage(List<Todo> todos) {
-		long completedCount = todos.stream().filter(todo -> todo.isCompleted()).count();
-		return ((double) completedCount / todos.size()) * 100;
-	}
-
-	@Test
-	void verifyAndExtractData() throws IOException {
-
-		Response response = RequestService.getUsers();
-		String responseBody = response.getBody().asString();
-		List<UserLocation> users = deserializeUserResponse(responseBody);
-
-		usersInFancodeCity = filterUsersByGeo(users);
-		Object[] userList = userDataProvider();
-
-		for (Object user : userList) {
-			System.out.println(user);
-		}
-
-		int actualStatusCode = response.statusCode();
-		Assert.assertEquals(actualStatusCode, 200);
-	}
-
-	@Test(dataProvider = "userDataProvider")
-	void verifyUsersInFancodeHave50PercentTasksComplete(int user) throws IOException {
+	@Test(dataProvider = "userDataProvider",dataProviderClass=UserDataProvider.class)
+	void verifyUsersFromCityFancodeHaveCompletedMoreThan50PercentTasks(int user) throws IOException {
+		//Given
 		Response response = RequestService.getTodo(user);
 		String responseBody = response.getBody().asString();
-		List<Todo> todos = deserializeTodoResponse(responseBody);
-		System.out.println(user);
-		double completedPercentage = calculateCompletedTaskPercentage(todos);
+		List<Todo> todos = DeserializeUtils.deserializeTodoResponse(responseBody);
+		//then
+		double completedPercentage = TodoUtils.calculateCompletedTaskPercentageForUsers(todos);
 		Assert.assertTrue(completedPercentage > 50);
 	}
 }
---
+++ src/test/java/com/fancode/tests/data/UserDataProvider.java
@@ -0,0 +1,43 @@
+package com.fancode.tests.data;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
+
+import com.fancode.api.RequestService;
+import com.fancode.models.UserLocation;
+import com.fancode.utils.DeserializeUtils;
+import com.fancode.utils.UserUtils;
+
+import io.restassured.response.Response;
+
+public class UserDataProvider {
+	 	
+	public List<UserLocation> getUsers() throws IOException{
+		Response response = RequestService.getUsers();
+		String responseBody = response.getBody().asString();
+		List<UserLocation> userLocations = DeserializeUtils.deserializeUserResponse(responseBody);
+		for(UserLocation userLocation:userLocations) {
+			System.out.println(userLocation);
+		}
+		return userLocations;
+	}
+	
+	public List<UserLocation> getUsersFromCity(double minLat,double maxLat,double minLng,double maxLng) throws IOException{
+		List<UserLocation> userLocations = getUsers();
+		
+		List<UserLocation> userLocationsInCity =  UserUtils.filterUsersByGeo(userLocations, minLat, maxLat, minLng, maxLng);
+		return userLocationsInCity;
+	}
+	
+	@DataProvider(name = "userDataProvider")
+	public Object[] userDataProvider() throws IOException {
+		int[] userIds = getUsersFromCity(-40,5,5,100).stream().mapToInt(UserLocation::getId).toArray();
+		Integer[] integerArray = Arrays.stream(userIds) 
+				.boxed() 
+				.toArray(Integer[]::new);
+		return Arrays.copyOf(integerArray, integerArray.length, Object[].class);
+	}
+}
